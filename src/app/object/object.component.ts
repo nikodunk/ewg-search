231bd3d4ed1a0bf589dc2bc543e4a426e7ce9744
nikodunk <n.dunkel@gmail.com>
2017-03-09 10:20:22 -0500

    brand results

--- src/app/object/object.component.ts
+++ src/app/object/object.component.ts
@@ -27,8 +27,9 @@ export class ObjectComponent implements OnInit {
   getResult(){
       // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query=patagonia%20surfing&filterImages=false';
       // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.searchQuery+'&filterImages=false';
-      let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.company.name+'&filterImages=false';
-      // this.getQuery()
+      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.company.name+'&filterImages=false';
+      this.makeBrand()
+      let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&brand[]='+this.firstNameCompany+'&filterImages=false'
       // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.firstNameCompany+'%20'+this.searchQuery+'&filterImages=false';
       // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.company.name+'%20'+this.searchQuery+'&filterImages=false';
       let reqHeaders = new Headers({ 'Authorization': '57b0201432561234baaf7c58fa7797fb70b5689f' });
@@ -41,9 +42,13 @@ export class ObjectComponent implements OnInit {
       // return resultItems
     }
 
-    // getQuery(){
-    //   this.firstNameCompany = this.company.name.substr(0, this.company.name.indexOf(" "))
-    // }
+    makeBrand(){
+      // this.firstNameCompany = this.company.name.substr(0, this.company.name.indexOf(" "))
+     this.firstNameCompany = this.company.name.replace("Company", "").replace("LLC", "").replace("L.L.C.", "").replace("Inc.", "").replace("Company", "").replace("Cosmetics", "").replace("'s", "").replace("Dr.", "Dr")
+     this.firstNameCompany = this.firstNameCompany.trim()
+     this.firstNameCompany = this.firstNameCompany.replace(/,/g , "").replace(/ /g, "%20")
+     console.log(this.firstNameCompany)
+    }
 
 
   ngOnInit() {
