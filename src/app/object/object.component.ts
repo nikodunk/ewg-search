624b592ebfc36ca5c3439d55234aa07abd7c9236
nikodunk <n.dunkel@gmail.com>
2017-03-18 18:01:29 -0400

    added sort

--- src/app/object/object.component.ts
+++ src/app/object/object.component.ts
@@ -13,63 +13,20 @@ declare var ga: any;
   templateUrl: './object.component.html',
   styleUrls: ['./object.component.css']
 })
-export class ObjectComponent implements OnInit {
+export class ObjectComponent {
 
 	@Input() searchQuery;
 	@Input() product;
-	resultItems = null;
   subscription: Subscription;
-  loader: string;
-  firstNameCompany: string;
 
 
   constructor(private http: Http) { }
 
-
-  getResult(){
-      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query=patagonia%20surfing&filterImages=false';
-      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.searchQuery+'&filterImages=false';
-      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.company.name+'&filterImages=false';
-      // this.makeBrand()
-      let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.product.name+'&itemsPerPage=1&filterImages=false'
-      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.firstNameCompany+'%20'+this.searchQuery+'&filterImages=false';
-      // let reqUrl = 'https://rest.viglink.com/api/product/search?apiKey=645728cd6d815e3890d53f3b16f562cd&query='+this.company.name+'%20'+this.searchQuery+'&filterImages=false';
-      let reqHeaders = new Headers({ 'Authorization': 'secret 57b0201432561234baaf7c58fa7797fb70b5689f' });
-      this.http.get(reqUrl, {headers: reqHeaders})
-               .map(res => {this.resultItems = res.json()})
-               .subscribe((data) => {console.log(this.resultItems)
-                                     this.loader = 'loaded';
-                                     })
-      // console.log('resultItems: ' + this.resultItems.items[2].name)
-      // return resultItems
-  }
-
-  // makeBrand(){
-  //   // this.firstNameCompany = this.company.name.substr(0, this.company.name.indexOf(" "))
-  //  this.firstNameCompany = this.product.name.replace("Company", "").replace("LLC", "").replace("L.L.C.", "").replace("Inc.", "").replace("Company", "").replace("Cosmetics", "").replace("'s", "").replace("Dr.", "Dr").replace("Group", "").replace("Ltd.", "")
-  //  this.firstNameCompany = this.firstNameCompany.trim()
-  //  this.firstNameCompany = this.firstNameCompany.replace(/,/g , "").replace(/ /g, "%20")
-  //  console.log(this.firstNameCompany)
-  // }
 
   googleAnalytics(){
     ga('send', 'event', 'Affiliate', this.product.name)
   }
 
 
-  ngOnInit() {
-      // let timer = Observable.timer(1500);
-      // this.subscription = timer.subscribe(t=> { 
-                                                 this.loader = 'loading';
-                                                 this.getResult();
-                                                 // console.log('now start getting request');
-                                                // });
-    };
-
-
-  ngOnDestroy() {
-      // this.subscription.unsubscribe();
-      this.resultItems = null;
-   }
 
 }
